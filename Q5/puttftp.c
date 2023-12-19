efa36c45e568def9f5ebb843a69d2d26e4027976
Antochen0321 <130651941+Antochen0321@users.noreply.github.com>
2023-12-19 02:12:33 +0100

    Update puttftp.c

--- Q5/puttftp.c
+++ Q5/puttftp.c
@@ -37,7 +37,7 @@ void send_wrq(int sock, const char *file) {
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_addr.s_addr = htonl(IPPROTO_UDP);
     server_addr.sin_port = htons(1069);
 
     // Use sendto to send the WRQ request
